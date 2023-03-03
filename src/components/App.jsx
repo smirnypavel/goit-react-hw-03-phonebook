d80d0139f851dc65b77ac30c2d1906212fa66d21
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-03-03 18:10:09 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -56,7 +56,19 @@ export class App extends Component {
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
